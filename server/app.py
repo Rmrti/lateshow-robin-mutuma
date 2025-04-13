b3826cf3c2082d679bc2f8814354a3fa1803fce1
Rmrti <mutuma34@gmail.com>
2025-04-13 17:31:44 +0300

    initialize flask and and write models

---
+++ server/app.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from flask_migrate import Migrate
+from flask import Flask, jsonify, request, make_response
+from flask_restful import Api, Resource
+
+import os
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
+
+db = SQLAlchemy(metadata=metadata)
+migrate = Migrate()
+
+class Config:
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'lateshow.db')}")
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.json.compact = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from server.models import Guest, Episode, Appearance
+
+
+    #Route for homepage
+    @app.route("/")
+    def index():
+        return "<h1>Code challenge</h1>"
+    
+    return app
+
+
+
+
+
+
+
+
+
+
+
---
+++ server/models.py
@@ -0,0 +1,75 @@
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+from .app import db
+
+
+
+
+class Guest(db.Model, SerializerMixin):
+    __tablename__ = "guests"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    occupation = db.Column(db.String)
+
+    # add relationship
+
+    appearances = db.relationship ('Appeareance' , backref = 'guest', cascade = "all, delete")
+    
+
+
+    # add serialization rules
+
+    episodes = association_proxy('appearances', 'episode')
+    serialize_rules = ('-appearances.guest',)
+
+    def __repr__(self):
+        return f"<Guest {self.name}>"
+
+
+
+class Episode(db.Model, SerializerMixin):
+    __tablename__ = "episodes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String)
+    number = db.Column(db.String)
+
+    # add relationship
+
+    appearances = db.relationship ("Appearance" , backref = 'episode', cascade = "all,delete")
+
+    # add serialization rules
+
+    guests = association_proxy('appearances', 'guest')
+    serialize_rules = ('-appearances.episode',)
+
+    def __repr__(self):
+        return f"<Episode {self.number}, {self.date}>"
+    
+class Appearance(db.Model, SerializerMixin):
+    __tablename__ = "appearances"
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+
+    # add relationships
+
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable= False)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable= False)
+
+    # add serialization rules
+
+    serialize_rules = ('-guests.appearances', '-episode.appearances')
+
+
+    # add validation
+    @validates('rating')
+    def validate_price(self, key, value):
+        if value < 1 or value > 5:
+            raise ValueError("Rating should be between 1 and 5")
+        return value
+        
+    def __repr__(self):
+        return f"<Appeareance ${self.rating}>"
